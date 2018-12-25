fb44fc3323dd486eae6c13983c02af592fbfcb34
chenva <25673578@qq.com>
2018-12-25 16:59:29 +0800

    1

--- src/main/java/com/himalayan/sbup/controller/FileUploadController.java
+++ src/main/java/com/himalayan/sbup/controller/FileUploadController.java
@@ -8,7 +8,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
-import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.File;
 import java.io.IOException;
--- src/test/java/com/himalayan/sbup/SbupApplicationTests.java
+++ src/test/java/com/himalayan/sbup/SbupApplicationTests.java
@@ -1,9 +1,7 @@
 package com.himalayan.sbup;
 
-import com.himalayan.sbup.controller.FileUploadController;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
